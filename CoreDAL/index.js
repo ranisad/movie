ad6f20a17298e1476d233fd3711ead27b81ca493
Sadhana Upadhyay <sadhana.upadhyay@ril.com>
2018-07-24 12:33:21 +0530

    save changes

--- CoreDAL/index.js
+++ CoreDAL/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //Set up default mongoose connection
 var mongoDB = 'mongodb://sadhana590:sa1234@ds247061.mlab.com:47061/battle_db';
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
